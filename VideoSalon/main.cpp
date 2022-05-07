35bfccfbe9d8e29d88c58c5965eab567749116ff
Alexander Korchak <105043706+Idanthyrsus@users.noreply.github.com>
2022-05-07 13:39:12 +0300

    Update main.cpp

--- VideoSalon/main.cpp
+++ VideoSalon/main.cpp
@@ -26,48 +26,6 @@ int main(int argc, const char * argv[])
         outToFile("Movie List", movie);
 
     }
-    
-//
-//    for (int i = 0; i < numMovies; i++)
-//    {
-//        cin.ignore();
-//        cout << "Enter movie name: ";
-//        getline(cin, movieList[i].name);
-//        cout << "Genre: ";
-//        getline(cin, movieList[i].genre);
-//        cout << "Year of release: ";
-//        cin >> movieList[i].year;
-//        cout << "Movie price: ";
-//        cin >> movieList[i].price;
-//        cout << endl;
-//
-////        cout << "\t---" << movieList[i].name << "---" << endl;
-////        cout << "Genre: \t\t\t" << movieList[i].genre << endl;
-////        cout << "Year of release:" << movieList[i].year << endl;
-////        cout << "Price: \t\t\t" << movieList[i].price << endl;
-////        cout << endl;
-//    }
-    
-//    cout << "Enter the name of the movie you are searching for: ";
-//    cin.ignore();
-//    getline(cin, movie.name);
-//
-//    for (int i = 0; i < numMovies; i++)
-//    {
-//        if (movieList[i].name == movie.name)
-//        {
-//            cout << "Here are the details of your movie: " << endl;
-//            cout << "\t---" << movieList[i].name << "---" << endl;
-//            cout << "Genre: \t\t\t" << movieList[i].genre << endl;
-//            cout << "Year of release:" << movieList[i].year << endl;
-//            cout << "Price: \t\t\t" << movieList[i].price << endl;
-//            cout << endl;
-//
-//        }
-//    }
-    
-   
-    
     if (!movieList)
     {
         cout << "Memory allocation error!";
